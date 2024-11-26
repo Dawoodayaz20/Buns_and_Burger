2b0713901a1537c69caf5f02970dd54c8ab9d667
Ch_Dawood Ayaz <dawoodayaz18@gmail.com>
2024-11-26 15:54:01 +0500

    Created Home page and item page

---
+++ Buns&Burger.js
@@ -0,0 +1,16 @@
+//slide show pictures function
+let slideindex = 0 
+showSlides()
+function showSlides() {
+    let i ;
+    let slides = document.getElementsByClassName('slideshow_pics');
+    for (i=0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    slideindex++;
+    if(slideindex > slides.length){
+        slideindex = 1;
+    }
+    slides[slideindex - 1].style.display = 'block';
+    setTimeout(showSlides, 3000) //This will call the function after every 3sec
+}
